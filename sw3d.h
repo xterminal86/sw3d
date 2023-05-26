8d29d0aefa4daf3a2e9fff5d805097fdc12b9abf
xterminal86 <xterminal86@gmail.com>
2023-05-27 01:55:28 +0300

    [codebase]
    - code comments
    - window is drawn in the center of the screen

--- sw3d.h
+++ sw3d.h
@@ -10,7 +10,7 @@
 namespace SW3D
 {
   using Clock = std::chrono::high_resolution_clock;
-  using Ns = std::chrono::nanoseconds;
+  using ns = std::chrono::nanoseconds;
 
   enum class TriangleType
   {
@@ -65,9 +65,16 @@ namespace SW3D
         _windowWidth  = windowWidth;
         _windowHeight = windowHeight;
 
+        SDL_DisplayMode dm;
+        if (SDL_GetCurrentDisplayMode(0, &dm) < 0)
+        {
+          SDL_Log("SDL_GetCurrentDisplayMode() error: %s", SDL_GetError());
+          return false;
+        }
+
         _window = SDL_CreateWindow(_windowName.data(),
-                                   0,
-                                   0,
+                                   dm.w / 2 - _windowWidth / 2,
+                                   dm.h / 2 - _windowHeight / 2,
                                    _windowWidth,
                                    _windowHeight,
                                    SDL_WINDOW_SHOWN);
@@ -116,7 +123,7 @@ namespace SW3D
         Clock::time_point measureStart;
         Clock::time_point measureEnd;
 
-        Ns dt = Ns{0};
+        ns dt = ns{0};
 
         while (_running)
         {
@@ -201,30 +208,6 @@ namespace SW3D
                         int x3, int y3,
                         uint32_t colorMask)
       {
-        //
-        // For any type of winding one invSlope will be negative, one positive.
-        //
-        // You can do the same with "classic" slope as well, but then
-        // you'll have to scanline vertically instead. I left the original
-        // algorithm I found in the Internet, since it's more natural to think
-        // of scanlines in terms of a line going from left to right,
-        // top to bottom.
-        //
-        // Since we're controlling y here, we have to find out next X coordinate
-        // when Y has increased by one. That's why we use inverted slope.
-        // Because k*x is the same as (k + k + k + k) x times, we can see that
-        // by solving for x we'll get x = y/k. So we can calculate 1/k
-        // beforehand and then just add it in every iteration of the loop.
-        //
-        // Since:
-        //
-        //      dy      1     1       1     dx
-        // k = ----,   --- = ----,   --- = ----
-        //      dx      k     dy      k     dy
-        //                   ----
-        //                    dx
-        //
-
         TriangleType tt = GetTriangleType(x1, y1, x2, y2, x3, y3);
 
         SwapCoords(x1, y1, x2, y2, x3, y3, tt);
@@ -247,22 +230,32 @@ namespace SW3D
             if (y3 > y2)
             {
               //
-              // x4 is derived from Intercept Theorem.
+              // In order to split composite triangle in two we need to find out
+              // the x coordinate of the new point that lies on the longest side
+              // (it's written as 4 here).
               //
-              //         1
-              //
-              //      2  o  4
-              //
-              //         x    3
+              // x4 is derived from Intercept Theorem:
               //
               // "The ratio of the two segments on the same ray starting at S
               // equals the ratio of the segments on the parallels:"
               //
-              // Thus, the following holds true:
+              // So, given this:
               //
-              // 1-o   o-4
-              // --- = ---
-              // 1-x   x-3
+              // +---------------> x
+              // |
+              // |        1
+              // |
+              // |     2  z  4
+              // |
+              // |        w    3
+              // ▼
+              // y
+              //
+              // the following holds true:
+              //
+              // [1 to z]   [z to 4]
+              // -------- = --------
+              // [1 to w]   [w to 3]
               //
               // which gives us:
               //
@@ -270,13 +263,12 @@ namespace SW3D
               // --------- = ---------
               // (y3 - y1)   (x3 - x1)
               //
-              // solving for x4:
               //
+              // Solving for x4 yields:
               //
               // (y2 - y1)
               // --------- * (x3 - x1) = (x4 - x1)
               // (y3 - y1)
-              //
               //
               //           (y2 - y1)
               // x4 = x1 + --------- * (x3 - x1)
@@ -292,16 +284,34 @@ namespace SW3D
             else if (y2 > y3)
             {
               //
-              // This variant is just a matter of flipping some signs and
-              // variables because of a mirror image of the above.
-              // But you can derive this in the same way.
+              // For mirror-image situation the process is the same.
               //
+              // +------------> x
+              // |
+              // |        1
+              // |
+              // |     4  z  3
+              // |
+              // |   2    w
+              // |
+              // ▼
+              // y
               //
-              //         1
+              // [1 to z]   [z to 4]
+              // -------- = --------
+              // [1 to w]   [w to 3]
               //
-              //      4  o  3
+              // (y3 - y1)   (x1 - x4)
+              // --------- = ---------
+              // (y2 - y1)   (x1 - x2)
               //
-              //    2    x
+              //             (y3 - y1)
+              // (x1 - x2) * --------- = (x1 - x4)
+              //             (y2 - y1)
+              //
+              //           (y3 - y1)
+              // x4 = x1 - --------- * (x1 - x2)
+              //           (y2 - y1)
               //
 
               x4 = x1 - ( (double)(y3 - y1) / (double)(y2 - y1) ) * (x1 - x2);
@@ -321,6 +331,8 @@ namespace SW3D
       {
         return _deltaTime;
       }
+
+      // -----------------------------------------------------------------------
 
     protected:
       std::string _windowName = "DrawService window";
@@ -402,6 +414,64 @@ namespace SW3D
                                   uint32_t colorMask,
                                   bool includeLastPoint = true)
       {
+        //
+        // For any type of winding one invSlope will be negative, one positive.
+        //
+        // You can do the same with "classic" slope as well, but then
+        // you'll have to scanline vertically instead. I left the original
+        // algorithm I found in the Internet, since it's more natural to think
+        // of scanlines in terms of a line going from left to right,
+        // top to bottom, like in CRT television.
+        //
+        // Since we're controlling y here, we have to find out next X coordinate
+        // when Y has increased by one. That's why we use inverted slope.
+        // Because k*x is the same as (k + k + k + k) x times, we can see that
+        // by solving for x we'll get x = y/k. So we can calculate 1/k
+        // beforehand and then just add it in every iteration of the loop.
+        //
+        // Since:
+        //
+        // y
+        //
+        // ^
+        // |     * p2
+        // |    /
+        // |   /
+        // |  /
+        // | * p1
+        // |
+        // +----------------------> x
+        //
+        // p1: (x1, y1), p2: (x2, y2)
+        //
+        // dy = (y2 - y1)
+        // dx = (x2 - x1)
+        //
+        //      dy      1     1       1     dx
+        // k = ----,   --- = ----,   --- = ----
+        //      dx      k     dy      k     dy
+        //                   ----
+        //                    dx
+        //
+        // In case of our triangle on the screen:
+        //
+        // +-----------------------> x
+        // |
+        // |
+        // |            p1 (x1, y1)
+        // |            / \
+        // |           /   \
+        // |          /     \
+        // | (x2, y2)p2-----p3 (x3, y3)
+        // |
+        // |
+        // ▼
+        // y
+        //
+        // It's easy to see, that one invSlope will be negative, thus no need
+        // to bother with + or - during increment, it will be handled
+        // automatically in += depending on the sign of the term.
+        //
         double invSlope1 = (double)(x2 - x1) / (double)(y2 - y1);
         double invSlope2 = (double)(x3 - x1) / (double)(y3 - y1);
 
@@ -417,10 +487,6 @@ namespace SW3D
             DrawPoint(lineX, scanline, colorMask);
           }
 
-          //
-          // Ladder-like movement to left and right respectively,
-          // depending on chosen winding, CCW in this project.
-          //
           curx1 += invSlope1;
           curx2 += invSlope2;
         }
@@ -434,6 +500,10 @@ namespace SW3D
                                uint32_t colorMask,
                                bool includeLastPoint = true)
       {
+        //
+        // The idea is exactly the same as above,
+        // just in this case we go from bottom to top.
+        //
         double invSlope1 = (double)(x2 - x1) / (double)(y2 - y1);
         double invSlope2 = (double)(x3 - x1) / (double)(y3 - y1);
 
@@ -452,6 +522,11 @@ namespace SW3D
             DrawPoint(lineX, scanline, colorMask);
           }
 
+          //
+          // And since we go up, we need to decrement y.
+          // Again, the sign will be handled automatically since this is
+          // basically the mirror image operation of the flat bottom triangle.
+          //
           curx1 -= invSlope1;
           curx2 -= invSlope2;
         }
@@ -470,6 +545,51 @@ namespace SW3D
                                    int x2, int y2,
                                    int x3, int y3)
       {
+        //
+        // There are 3 cases of 2D triangles.
+        //
+        // BASIC:
+        // ------
+        //
+        // 1) Flat-top
+        //
+        //       1
+        //
+        //     2   3
+        //
+        // 2) Flat-bottom:
+        //
+        //     3   2
+        //
+        //       1
+        //
+        // SPECIAL:
+        // --------
+        //
+        // 3) Composite, which comes in two forms:
+        //
+        //       1              1
+        //
+        //     2        ->    2   3
+        //                      +
+        //          3         3     2
+        //
+        //                            1
+        //
+        // Its mirror image:
+        //
+        //     1                1
+        //
+        //        3     ->    2   3
+        //                      +
+        //  2               3     2
+        //
+        //
+        //                1
+        //
+        //
+        // and which can be split into two basic ones.
+        //
         bool isFlatBottom = ( (y2 == y3) and (y1 < y2) ) or
                             ( (y1 == y2) and (y3 < y2) ) or
                             ( (y3 == y1) and (y2 < y3) );
@@ -490,10 +610,35 @@ namespace SW3D
                       int& x3, int& y3,
                       TriangleType tt)
       {
+        //
+        // There are two main methods of specifying the order of vertices:
+        // ClockWise (CW) and Counter ClockWise (CCW). Its main purpose
+        // is to determine which faces are considered "front" faces,
+        // i.e. the ones that should be drawn.
+        //
+        // For a simple drawing of a 2D filled triangle on the screen
+        // winding doesn't matter, but it will matter if you plan to do
+        // shading and back face culling for example.
+        // At design phase you can choose CW or CCW winding order,
+        // it won't make a difference for the end result, as long as you're
+        // consistent with it.
+        //
+        // We'll be using CCW in this project.
+        //
+        // So we'll be assuming that the user is responsible for supplying
+        // vertices in the correct winding order for the object.
+        // But it may be possible to specify points in the correct winding order
+        // but different relative order. Our rasterization algorithms assume
+        // that triangle points have *specific* relative order, so if user
+        // specified points in the wrong relative order, rasterization will fail.
+        // So we need to account for that by swapping the corresponding coordinates.
+        // Finding out if relative order of points was wrong is just a matter of
+        // bruteforcing the coordinates' location against one another.
+        //
         switch (tt)
         {
           //
-          // Proper winding:
+          // Proper order:
           //
           //       1
           //
@@ -529,7 +674,7 @@ namespace SW3D
           break;
 
           //
-          // Proper winding:
+          // Proper order:
           //
           //    3   2
           //
@@ -564,8 +709,17 @@ namespace SW3D
           }
           break;
 
+          //
+          // Since we'll be splitting this triangle in two later on there is
+          // no proper relative order for this one, but we can prepare certain
+          // relative order beforehand just to make everything sort of standard.
+          // So when we'll be splitting this in two later we can be sure that
+          // point 1 is always at the top, point 2 is to the left and point 3 is
+          // to the right.
+          //
           case TriangleType::COMPOSITE:
           {
+            //
             //
             //      3           1
             //
